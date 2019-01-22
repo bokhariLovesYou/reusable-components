ca0fcb872925f27b245d7860c950eacb97305cf1
Paras Bokhari <parasbokhari@gmail.com>
2019-01-22 13:31:53 -0600

    Updating Description of the file

--- reusablefunctions.js
+++ reusablefunctions.js
@@ -1,3 +1,9 @@
+// These are some functions which can be called again and again. Some of these also can be used to create newer functions as the functionality can be shared.
+// Just created this file so that I can start storing any reusable functions I have created and reuse them when needed.
+
+// Please note: Most of these functions require refactoring - therefore please feel free to make them better.
+
+
 // **************** // ***************** //
 // **************** // ***************** //
 
